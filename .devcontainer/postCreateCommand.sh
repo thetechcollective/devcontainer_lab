78d398ed43c2de72e79ce41f781668a4161a6873
Lars Kruse <lakruzz@inc-inc.dk>
2025-01-17 13:02:05 +0000

    azure

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-echo "Running $0"
+PREFIX="$(basename $0): "
 
 
 if [ "$GITHUB_CLI_AUTH_REQUIRED" = "1" ]; then
-    echo "GitHub CLI authentication is required"
+    echo "$PREFIX GitHub CLI authentication is required"
     if [ -n "$GITHUB_TOKEN_BASE64" ]; then
-        echo "...using token from host"
+        echo "$PREFIX ...using token from host"
         echo $GITHUB_TOKEN_BASE64 | base64 --decode | gh auth login --with-token
         sed -i '/^GITHUB_TOKEN_BASE64/d' $(dirname $0)/devcontainer.env
     else
-        gh auth login
+        gh auth login -w -p https -h github.com
     fi
 fi
 
-echo "Setting up git configuration to support .gitconfig in repo-root"
+echo "$PREFIX Setting up git configuration to support .gitconfig in repo-root"
 git config --local --get include.path | grep -e ../.gitconfig >/dev/null 2>&1 || git config --local --add include.path ../.gitconfig
 
 if [ -f Pipfile ]; then
-    echo "Installing Python --dev dependencies"
+    echo "$PREFIX Installing Python --dev dependencies"
     pipenv install --dev
 fi
